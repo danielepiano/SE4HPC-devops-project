ccffaacea982d34346d897ef10e16c1a8720df62
Daniele Piano <dpiano01@gmail.com>
2024-05-29 13:52:20 +0200

    test :: scalar associative property

--- test/test_matrix_multiplication.cpp
+++ test/test_matrix_multiplication.cpp
@@ -4,8 +4,12 @@
 #include <gtest/gtest.h>
 
 
-// ######################### Source code of multiplyMatrices in src/matrix_mult
-
+/*
+ * #####################################################################################################################
+ * Testing matrix product.
+ * A x B = C
+ * #####################################################################################################################
+ */
 TEST(MatrixMultiplicationTest, TestMultiplyMatrices) {
     std::vector<std::vector<int>> A = {
         {1, 2, 3},
@@ -26,38 +30,7 @@ TEST(MatrixMultiplicationTest, TestMultiplyMatrices) {
         {139, 154}
     };
 
-    ASSERT_EQ(C, expected) << "Matrix multiplication test failed! :(((((";
-}
-
-/*
- * #####################################################################################################################
- * Testing neutral element property (identity matrix on left).
- * I x B = B
- * #####################################################################################################################
- */
-TEST(MatrixMultiplicationTest, TestLeftIdentityMatrix) {
-    std::vector<std::vector<int>> A = {
-            {1, 0, 0},
-            {0, 1, 0},
-            {0, 0, 1}
-    };
-    std::vector<std::vector<int>> B = {
-            {1, 2, 3},
-            {4, 5, 6},
-            {7, 8, 9}
-    };
-
-    std::vector<std::vector<int>> C(2, std::vector<int>(2, 0));
-
-    multiplyMatrices(A, B, C, 2, 3, 2);
-
-    std::vector<std::vector<int>> expected = {
-            {1, 2, 3},
-            {4, 5, 6},
-            {7, 8, 9}
-    };
-
-    ASSERT_EQ(C, expected) << "Multiplication with left identity matrix failed! :(((((";
+    ASSERT_EQ(C, expected) << "Matrix product test failed! :(((((";
 }
 
 /*
@@ -146,6 +119,49 @@ TEST(MatrixMultiplicationTest, TestNeutralElementScalarProduct) {
 
     multiplyMatrices(B, A, C, 1, 1, 1);
     ASSERT_EQ(C, expected) << "Neutral element property (right 1) for scalar product failed! :(((((";
+}
+
+/*
+ * #####################################################################################################################
+ * Testing associative property for scalar product.
+ * a x b x c = (a x b) x c = a x (b x c) = (a x c) x b = abc = d
+ * #####################################################################################################################
+ */
+TEST(MatrixMultiplicationTest, TestAssociativePropertyScalarProduct) {
+    std::vector<std::vector<int>> A = {
+            {2}
+    };
+    std::vector<std::vector<int>> B = {
+            {3}
+    };
+    std::vector<std::vector<int>> C = {
+            {4}
+    };
+
+    std::vector<std::vector<int>> D_temp(1, std::vector<int>(1, 0));
+    std::vector<std::vector<int>> D1(1, std::vector<int>(1, 0));
+    std::vector<std::vector<int>> D2(1, std::vector<int>(1, 0));
+    std::vector<std::vector<int>> D3(1, std::vector<int>(1, 0));
+
+    // D1 = (A x B) x C
+    multiplyMatrices(A, B, D_temp, 1, 1, 1);
+    multiplyMatrices(D_temp, C, D1, 1, 1, 1);
+
+    // D2 = A x (B x C)
+    multiplyMatrices(B, C, D_temp, 1, 1, 1);
+    multiplyMatrices(A, D_temp, D2, 1, 1, 1);
+
+    // D3 = (A x C) x B
+    multiplyMatrices(A, C, D_temp, 1, 1, 1);
+    multiplyMatrices(D_temp, B, D3, 1, 1, 1);
+
+    std::vector<std::vector<int>> expected = {
+            {24}
+    };
+
+    ASSERT_EQ(D1, expected) << "Associative property ((A x B) x C) for scalar product failed! :(((((";
+    ASSERT_EQ(D2, expected) << "Associative property (A x (B x C)) for scalar product failed! :(((((";
+    ASSERT_EQ(D3, expected) << "Associative property ((A x C) x B) for scalar product failed! :(((((";
 }
 
 
