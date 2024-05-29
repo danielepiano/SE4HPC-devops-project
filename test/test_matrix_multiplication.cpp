b3f676af2b3ab146857f78ef95c9289628f95717
Daniele Piano <dpiano@soldo.com>
2024-05-29 13:35:35 +0200

    test :: scalar product, 0-element, neutral element

--- test/test_matrix_multiplication.cpp
+++ test/test_matrix_multiplication.cpp
@@ -62,34 +62,92 @@ TEST(MatrixMultiplicationTest, TestLeftIdentityMatrix) {
 
 /*
  * #####################################################################################################################
- * Testing neutral element property  (identity matrix on left).
- * A x I = A
+ * Testing scalar product.
+ * a x b = ab
  * #####################################################################################################################
  */
-TEST(MatrixMultiplicationTest, TestRightIdentityMatrix) {
+TEST(MatrixMultiplicationTest, TestScalarProduct) {
     std::vector<std::vector<int>> A = {
-            {1, 0, 0},
-            {0, 1, 0},
-            {0, 0, 1}
+            {7}
     };
     std::vector<std::vector<int>> B = {
-            {1, 2, 3},
-            {4, 5, 6},
-            {7, 8, 9}
+            {5}
     };
 
-    std::vector<std::vector<int>> C(2, std::vector<int>(2, 0));
+    std::vector<std::vector<int>> C(1, std::vector<int>(1, 0));
 
-    multiplyMatrices(A, B, C, 2, 3, 2);
+    multiplyMatrices(A, B, C, 1, 1, 1);
 
     std::vector<std::vector<int>> expected = {
-            {1, 2, 3},
-            {4, 5, 6},
-            {7, 8, 9}
+            {35}
     };
 
-    ASSERT_EQ(C, expected) << "Multiplication with right identity matrix failed! :(((((";
+    ASSERT_EQ(C, expected) << "Scalar product failed! :(((((";
+
+    multiplyMatrices(B, A, C, 1, 1, 1);
+    ASSERT_EQ(C, expected) << "Commutative scalar product failed! :(((((";
 }
+
+/*
+ * #####################################################################################################################
+ * Testing 0-element property for scalar product.
+ * 0 x b = 0
+ * Includes: metamorphic relation to test commutative property and so 0-product for both factors A and B.
+ * a x 0 = 0
+ * #####################################################################################################################
+ */
+TEST(MatrixMultiplicationTest, Test0ElementScalarProduct) {
+    std::vector<std::vector<int>> A = {
+            {0}
+    };
+    std::vector<std::vector<int>> B = {
+            {7}
+    };
+
+    std::vector<std::vector<int>> C(1, std::vector<int>(1, 0));
+
+    multiplyMatrices(A, B, C, 1, 1, 1);
+
+    std::vector<std::vector<int>> expected = {
+            {0}
+    };
+
+    ASSERT_EQ(C, expected) << "0-element property (left 0) for scalar product failed! :(((((";
+
+    multiplyMatrices(B, A, C, 1, 1, 1);
+    ASSERT_EQ(C, expected) << "0-element property (right 0) for scalar product failed! :(((((";
+}
+
+/*
+ * #####################################################################################################################
+ * Testing neutral element property for scalar product.
+ * 1 x b = b
+ * Includes: metamorphic relation to test commutative property and so 1-product for both factors A and B.
+ * a x 1 = a
+ * #####################################################################################################################
+ */
+TEST(MatrixMultiplicationTest, TestNeutralElementScalarProduct) {
+    std::vector<std::vector<int>> A = {
+            {1}
+    };
+    std::vector<std::vector<int>> B = {
+            {7}
+    };
+
+    std::vector<std::vector<int>> C(1, std::vector<int>(1, 0));
+
+    multiplyMatrices(A, B, C, 1, 1, 1);
+
+    std::vector<std::vector<int>> expected = {
+            {7}
+    };
+
+    ASSERT_EQ(C, expected) << "Neutral element property (left 1) for scalar product failed! :(((((";
+
+    multiplyMatrices(B, A, C, 1, 1, 1);
+    ASSERT_EQ(C, expected) << "Neutral element property (right 1) for scalar product failed! :(((((";
+}
+
 
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
